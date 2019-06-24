bc2b45b92ed9523844e5e79278e85e1fa713b7e7
Betarabbit14 <1048343184@qq.com>
2019-06-24 13:42:08 +0800

    added so much lmao

---
+++ c#CodePlatformer/Assets/noCodeAssetPack/Scripts/PlayerMovement.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+	public Animator animator;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+	bool crouch = false;
+
+	// Update is called once per frame
+	void Update () {
+
+        //respawnCheck
+        if (transform.position.y < -30)
+        {
+            Respawn();
+        }
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+			animator.SetBool("IsJumping", true);
+		}
+
+		if (Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		} else if (Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+
+	}
+
+    public void Respawn()
+    {
+        var startPos = new Vector2(-1,2);
+        transform.position = startPos;
+    }
+	public void OnLanding ()
+	{
+        Debug.Log("landed!");
+		animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching (bool isCrouching)
+	{
+        Debug.Log("crouching");
+		animator.SetBool("IsCrouching", isCrouching);
+	}
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
